f6ecae9b5414209813a3705473f9950149aa76ec
peng-jinbo <1277258389@qq.com>
2021-02-15 20:53:22 +0800

    2021.02.15

---
+++ 739. 每日温度/739.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        int n = T.size();
+        vector<int> ans(n);
+        stack<int> s;
+        for (int i = 0; i < n; ++i) {
+            while (!s.empty() && T[i] > T[s.top()]) {
+                int previousIndex = s.top();
+                ans[previousIndex] = i - previousIndex;
+                s.pop();
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
